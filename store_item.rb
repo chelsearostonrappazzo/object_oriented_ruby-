bca8dc48431f4e2eee6177d9add98b9624100411
Chelsea Roston <cmroston@gmail.com>
2021-02-17 13:31:39 -0500

    adds evening gown

--- store_item.rb
+++ store_item.rb
@@ -1,3 +1,3 @@
-item1 = { name: " ", color: " ", price: " " }
-item2 = { name: " ", color: " ", price: " " }
+item1 = { name: "regency evening gown", color: "mazarine blue", price: 465 }
+item2 = { name: "", color: " ", price: " " }
 item3 = { :name => " ", :color => " ", :price => " " }
