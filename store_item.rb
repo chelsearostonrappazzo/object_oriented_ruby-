e65f7059218d27a73c519040bdb0022fa59e6580
Chelsea Roston <cmroston@gmail.com>
2021-02-17 13:32:15 -0500

    adds promenade gown

--- store_item.rb
+++ store_item.rb
@@ -1,3 +1,3 @@
 item1 = { name: "regency evening gown", color: "mazarine blue", price: 465 }
-item2 = { name: "", color: " ", price: " " }
+item2 = { name: "regency promenade gown", color: "sprigged muslin", price: 200 }
 item3 = { :name => " ", :color => " ", :price => " " }
