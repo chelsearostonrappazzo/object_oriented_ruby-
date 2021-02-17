26bce3a7916b0e3b9d1ca3567d40fbaeddd5f929
Chelsea Roston <cmroston@gmail.com>
2021-02-17 14:11:08 -0500

    creates class for items

--- store_item.rb
+++ store_item.rb
@@ -1,3 +1,6 @@
 item1 = { name: "regency evening gown", color: "mazarine blue", price: 465 }
 item2 = { name: "regency promenade gown", color: "sprigged muslin", price: 200 }
 item3 = { :name => "regency mourning gown", :color => "onyx black", :price => 325 }
+
+class Item
+end
