08825aeefa773b16f4d9736d773e8c7a68d39cc9
Chelsea Roston <cmroston@gmail.com>
2021-02-17 14:13:36 -0500

    initializes variables

--- store_item.rb
+++ store_item.rb
@@ -3,4 +3,9 @@ item2 = { name: "regency promenade gown", color: "sprigged muslin", price: 200 }
 item3 = { :name => "regency mourning gown", :color => "onyx black", :price => 325 }
 
 class Item
+  def initialize(input_name, input_color, input_price)
+    @name = input_name
+    @color = input_color
+    @price = input_price
+  end
 end
