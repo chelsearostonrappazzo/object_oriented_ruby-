e883b0c06f36f2af97dd08203e29956d855f4caf
Chelsea Roston <cmroston@gmail.com>
2021-02-17 14:35:41 -0500

    changes to reader/writer shortcuts

--- store_item.rb
+++ store_item.rb
@@ -3,21 +3,15 @@ item2 = { name: "regency promenade gown", color: "sprigged muslin", price: 200 }
 item3 = { :name => "regency mourning gown", :color => "onyx black", :price => 325 }
 
 class Item
+  attr_reader: :name, :color, :price
+  attr_writer: :price
+
   def initialize(input_name, input_color, input_price)
     @name = input_name
     @color = input_color
     @price = input_price
   end
 
-  def name
-    @name
-  end
-
-  def color
-    @color
-  end
-
-  def price
-    @price
-  end
 end
+
+item1 = Item.new("regency evening gown", "mazarine blue", 465)
